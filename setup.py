2850bab481ed458b5c3977220a7a79d0b6f14980
Tejaswy Muralikrishna <tejaswy.muralikrishna@altair.com>
2022-09-19 14:59:20 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 # Metadata goes in pyproject.toml.
 # These are here for GitHub's dependency graph.
 setup(
-    name="nats-py",
-    extras_require={
-        'nkeys': ['nkeys']
-    }
+    name='nats-py',
+    version='1.0.0',
+    description='Nats using nkeys binary built from nkeys go project',
+    packages=find_packages(exclude=["./examples","./tests"]),
+    package_data={'': ['bin/win64/nk.exe']},
+    include_package_data=True,
+    install_requires=[],
 )
